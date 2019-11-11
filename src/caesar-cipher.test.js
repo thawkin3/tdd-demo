287f4e0cebe6606708716c7ff60b5eedcd39e2b7
Tyler Hawkins <thawkins@instructure.com>
2019-11-11 13:32:43 -0700

    adds test cases based on requirements

---
+++ src/caesar-cipher.test.js
@@ -0,0 +1,31 @@
+import { encode } from './caesar-cipher'
+
+describe('caesar cipher', () => {
+  it('takes a string and a shift value and returns a new string', () => {
+
+  })
+
+  it('shifts the A-Z characters by the correct amount', () => {
+
+  })
+
+  it('does not affect non-alphabetic characters', () => {
+
+  })
+
+  it('maintains case', () => {
+
+  })
+
+  it('handles wrapping past the end of the alphabet', () => {
+
+  })
+
+  it('handles shift values greater than 26', () => {
+
+  })
+
+  it('handles bad input', () => {
+
+  })
+})
