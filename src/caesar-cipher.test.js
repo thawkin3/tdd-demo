3e11e41b6c0f8e2f924cd0913478bab792ee0096
Tyler Hawkins <thawkins@instructure.com>
2019-11-19 09:57:19 -0700

    starting point with empty source code and tests

--- src/caesar-cipher.test.js
+++ src/caesar-cipher.test.js
@@ -2,40 +2,34 @@ import { encode } from './caesar-cipher'
 
 describe('caesar cipher', () => {
   it('takes a string and a shift value and returns a new string', () => {
-    expect(typeof(encode('abc', 1))).toBe('string')
+
   })
 
-  it('shifts the A-Z characters by the correct amount', () => {
-    expect(encode('abc', 1)).toBe('bcd')
-    expect(encode('test', 2)).toBe('vguv')
-  })
+  // it('shifts the A-Z characters by the correct amount', () => {
 
-  it('does not affect non-alphabetic characters', () => {
-    expect(encode('abc123', 1)).toBe('bcd123')
-  })
+  // })
 
-  it('maintains case', () => {
-    expect(encode('aBc', 1)).toBe('bCd')
-  })
+  // it('does not affect non-alphabetic characters', () => {
 
-  it('handles wrapping past the end of the alphabet', () => {
-    expect(encode('xyz', 2)).toBe('zab')
-  })
+  // })
 
-  it('handles shift values greater than 26', () => {
-    expect(encode('abc', 26)).toBe('abc')
-    expect(encode('abc', 28)).toBe('cde')
-  })
+  // it('maintains case', () => {
 
-  it('handles shift values less than 0', () => {
-    expect(encode('abc', 0)).toBe('abc')
-    expect(encode('abc', -2)).toBe('yza')
-  })
+  // })
 
-  it('handles bad input', () => {
-    expect(encode()).toBe('')
-    expect(encode(1, 1)).toBe('')
-    expect(encode(1, 'abc')).toBe('')
-    expect(encode('abc')).toBe('abc')
-  })
+  // it('handles wrapping past the end of the alphabet', () => {
+
+  // })
+
+  // it('handles shift values greater than 26', () => {
+
+  // })
+
+  // it('handles shift values less than 0', () => {
+
+  // })
+
+  // it('handles bad input', () => {
+
+  // })
 })
